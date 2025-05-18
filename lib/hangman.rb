4a70aea2fa43c571feece2471a5d502cc9200c84
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-18 21:45:43 +0100

    chore: Adjust git settings

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,1 +1,11 @@
 # frozen_string_literal: true
+
+# Hangman class
+# @author Ancient Nimbus
+# @since 0.1.0
+# @version 1.0.0
+class Hangman
+  def initialize
+    puts "Hello from Hangman class!"
+  end
+end
--- main.rb
+++ main.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 
+require_relative "lib/hangman"
 # Hangman Ruby
+# @author Ancient Nimbus
+# @since 0.1.0
+# @version 1.0.0
+def main
+  Hangman.new
+end
+
+main
