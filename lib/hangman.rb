a0f119c85f01575e5b053912f248325b3cbba7d3
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-19 16:33:07 +0100

    feat: Update game save architecture
    This iteration improves the robustness and ease of access when reading
    game sessions data.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -9,19 +9,47 @@ require_relative "cli_helper"
 # Hangman class
 # @author Ancient Nimbus
 # @since 0.1.0
-# @version 1.0.1
+# @version 1.0.2
 class Hangman
   include FileUtils
 
-  attr_reader :dict_path
+  attr_reader :dict_path, :p1
 
   def initialize(dict_path: FileUtils.assets_path("dictionary.txt"))
     @dict_path = dict_path
     @p1 = Player.new
+    # game_loop
     test
   end
 
+  def game_loop
+    round = 6
+
+    round.times do |idx|
+      puts "#{round - idx} round#{'s' if round - idx >= 1}"
+      test
+      gets
+    end
+  end
+
+  private
+
   def test
-    puts FileUtils.random_word(dict_path)
+    # puts FileUtils.random_word(dict_path)
+    puts p1.resume_session
   end
 end
+
+# @todo resume game state from load save
+# @todo option to choose new game and load game
+# @todo option to save file
+# @todo auto save toggle
+# @todo game mode selection
+# @todo core game loop
+# @todo CLI input handling
+# @todo CLI command handling
+# @todo CLI user experience
+# @todo CLI display game state
+# @todo CLI how to play
+# @todo CLI ASCII art
+# @todo
--- lib/player.rb
+++ lib/player.rb
@@ -1,19 +1,42 @@
 # frozen_string_literal: true
 
+require_relative "file_utils"
+FUS = FileUtils
 # Player class
+# @version 1.0.1
 class Player
+  include FUS
+
   attr_accessor :name, :savefile
 
   def initialize(name: "Spock")
     @name = name
-    @savefile = create_savefile
+    new_save
   end
 
-  def create_savefile
-    { saved_date: Time.now.ceil, name: name, hangman_data: {
-      mode: "standard",
-      game1: { word_id: 123, remaining_lives: 3, state: ["a", "", "", "", "e"] },
-      game2: { word_id: 456, remaining_lives: 5, state: ["", "o", "a", "", ""] }
+  # @since 0.1.1
+  # @version 1.1.0
+  def new_save
+    @savefile = { saved_date: Time.now.ceil, name: name, hangman_data: {
+      mode: "standard", sessions: [
+        { id: 1, word_id: 123, remaining_lives: 3, state: %w[a _ p l e], status: :ended, win?: true },
+        { id: 2, word_id: 456, remaining_lives: 0, state: %w[_ o a _ _], status: :ended, win?: false },
+        { id: 3, word_id: 789, remaining_lives: 5, state: %w[_ o a _ _], status: :active, win?: false }
+      ]
     } }
+    FUS.write_savefile(FUS.data_path(name), savefile)
+  end
+
+  def save_game(session_obj)
+    savefile[:hangman_data].store(:"game#{}")
+  end
+
+  def load_game; end
+
+  # Find the latest session
+  # @since 0.1.3
+  # @version 1.0.0
+  def resume_session
+    savefile.dig(:hangman_data, :sessions).bsearch { |game| game[:status] == :active }
   end
 end
