1a9045d18ee1e983ec1567bee860f48089c80de6
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-19 18:30:52 +0100

    refactor: Improve getter and setter usage

--- lib/player.rb
+++ lib/player.rb
@@ -13,49 +13,43 @@ class Player
     @name = name
     @mode = mode
     new_save
-    @sessions = @savefile.dig(:hangman_data, :sessions)
+    @sessions = savefile.dig(:hangman_data, :sessions)
     @session_counts = sessions.size
   end
 
   # @since 0.1.1
-  # @version 1.1.1
+  # @version 1.1.2
   def new_save
-    @savefile = { saved_date: Time.now.ceil, name: @name, hangman_data: { mode: @mode, sessions: [] } }
+    @savefile = { saved_date: Time.now.ceil, name: name, hangman_data: { mode: mode, sessions: [] } }
     overwrite_save
   end
 
   # Write game save to disk
   # @since 0.1.4
-  # @version 1.0.0
+  # @version 1.0.1
   def overwrite_save
-    FUS.write_savefile(FUS.data_path(@name), @savefile)
+    FUS.write_savefile(FUS.data_path(name), savefile)
   end
 
   # Load save from player's disk
   # @since 0.1.4
-  # @version 1.0.0
+  # @version 1.0.1
   def load_save
-    @savefile = FUS.load_savefile(FUS.data_path("spock_dup"))
+    self.savefile = FUS.load_savefile(FUS.data_path(name))
   end
 
   # Add latest session to savefile and store game save to disk
   # @since 0.1.4
   # @version 1.0.1
   def save_game(session_obj)
-    @sessions.push(session_obj)
+    sessions.push(session_obj)
     overwrite_save
   end
 
   # Find the latest session
   # @since 0.1.3
-  # @version 1.0.1
+  # @version 1.0.2
   def resume_session
-    @sessions.bsearch { |game| game[:status] == :active }
+    sessions.bsearch { |game| game[:status] == :active }
   end
-
-  # # @since 0.1.4
-  # # @version 1.0.0
-  # def session_counts
-  #   @sessions.size
-  # end
 end
