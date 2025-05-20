a78493183b528ce9528a6a043981de6c85797dcf
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-20 16:29:42 +0100

    feat: Improve console command flow and handling

--- lib/console.rb
+++ lib/console.rb
@@ -28,32 +28,59 @@ class Console
   # @version 1.0.0
   def run
     puts t("welcome.greeting").colorize(:green)
-
+    # process_input(user_input, regexp_on: true, reg: /\A[1-2]\z/) regex ver
     process_input(user_input) while app_running
   end
 
   # Shorthand for FUS.t() method
   # @since 0.1.8
-  # @version 1.0.0
-  def t(key_path, swaps = {}, locale: "en")
-    FUS.t(key_path, swaps, locale: locale)
+  # @version 1.1.0
+  def t(key_path, swaps = {}, locale: "en", prefix: "* ")
+    prefix + FUS.t(key_path, swaps, locale: locale)
   end
 
   # Get user input
-  def user_input
-    print t("console.prompt_prefix")
+  # @since 0.1.8
+  # @version 1.1.0
+  def user_input(msg = t("console.prompt"))
+    puts msg
+    print t("console.input_prefix", prefix: "")
     gets.chomp
   end
 
   # param input [String]
   # @since 0.1.8
-  # @version 1.0.0
-  def process_input(input)
-    parts = input.downcase.split(" ")
-    command = parts[0]
-    args = parts[1..] || []
+  # @version 1.1.0
+  def process_input(input, regexp_on: false, reg: /.*/)
+    is_command = input[0..1] == "--"
+    parts = input.split(" ")
 
+    if is_command
+      args = parts[1..] || []
+      process_command(parts[0], args)
+    elsif regexp_on
+      process_regexp(input, reg)
+    else
+      input
+    end
+  end
+
+  # @since 0.1.9
+  # @version 1.0.0
+  def process_command(command, args = [])
+    command = command.downcase.gsub("-", "")
     commands.key?(command) ? commands[command].call(args) : puts(t("error.cmd_not_found", { command: command }))
+    command
+  end
+
+  # @since 0.1.9
+  # @version 1.0.0
+  def process_regexp(input, reg)
+    until input.match?(reg) && !input.empty?
+      # puts "User has entered: #{input}"
+      input = process_input(user_input(t("console.invalid_warning"))) # print on second entry
+    end
+    input
   end
 
   private
