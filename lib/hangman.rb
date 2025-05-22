793f6d6c2e6a1ca57b4226ba63491739fa7a4775
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-22 15:43:32 +0100

    refactor: Optimise various methods in hangman.rb
    Perform rubocop checks and reduce the line lengths of several methods.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -7,12 +7,12 @@ require_relative "player"
 # Hangman class
 # @author Ancient Nimbus
 # @since 0.1.0
-# @version 1.2.0
+# @version 1.3.0
 class Hangman
   include FUS
 
-  attr_accessor :p1, :load_save, :active_session, :play_set, :lives, :c_char
-  attr_reader :cli, :dict_path, :s_word_obj, :secret_word
+  attr_accessor :p1, :load_save, :active_session, :play_set, :lives
+  attr_reader :cli, :dict_path, :s_word_obj, :s_word
 
   # Game mode configurations
   MODE = { 1 => 7, 2 => 6, 3 => 5 }.freeze
@@ -24,19 +24,13 @@ class Hangman
     @active_session = nil
     @p1 = Player.new(console)
 
-    boot_screen
+    # Boot_screen
+    puts cli.t("boot", prefix: "").colorize(:yellow)
 
     @load_save = cli.load_session
-
     player_profile(load_save)
-
     resume_session
-
     init_game
-  end
-
-  def boot_screen
-    puts cli.t("boot", prefix: "").colorize(:yellow)
   end
 
   # Set the player of the play session.
@@ -65,25 +59,23 @@ class Hangman
   end
 
   # @since 0.1.4
-  # @version 1.6.0
+  # @version 1.7.0
   def init_game
     self.active_session ||= create_session
     @lives = active_session[:remaining_lives]
     @play_set = active_session[:play_set]
-    @secret_word = word_lookup(active_session[:word_id])
-    @c_char = ""
+    @s_word = word_lookup(active_session[:word_id])
     p1.save_game(active_session)
 
-    # Initial display
     print_session(first: true)
-    # Enter game loop
+
     game_loop
   end
 
   # @since 0.1.6
-  # @version 1.4.0
+  # @version 1.5.0
   def print_session(first: true)
-    # puts secret_word
+    puts s_word
     # display blanks
     puts "\n* #{active_session[:state].join(' ').colorize(:light_blue)}"
     # display gallows
@@ -91,28 +83,24 @@ class Hangman
   end
 
   # @since 0.3.5
-  # @version 1.3.0
+  # @version 1.4.0
   def print_gallows(first: false)
     first = false if lives != MODE[p1.mode]
-    # format_row = ->(r_num, min, max, set = play_set) { { "r#{r_num}" => set[min..max].upcase.chars.join(" ") } }
-    format_rows = lambda { |r_num, min, max, set = play_set|
-      count = 0
-      obj = {}
-      r_num.times do
-        obj["r#{count + 1}"] = set[min[count]..max[count]].upcase.chars.join(" ")
-        count += 1
-      end
-      obj
-    }
-    puts
-    puts FUS.t("hm.gallows.#{first ? 7 : lives}", format_rows.call(6, [0, 3, 7, 13, 19, 23], [2, 6, 12, 18, 22, 25]))
+    puts FUS.t("hm.gallows.#{first ? 7 : lives}", format_alphabet_grid([0..2, 3..6, 7..12, 13..18, 19..22, 23..25]))
+  end
+
+  # @since 1.3.0
+  # @version 1.0.0
+  def format_alphabet_grid(layout)
+    layout.map.with_index(1) do |range, row_num|
+      ["r#{row_num}", play_set[range].upcase.chars.join(" ")]
+    end.to_h
   end
 
   # @since 0.2.0
-  # @version 1.3.0
+  # @version 1.4.0
   def make_guess
-    self.c_char = cli.process_input(cli.user_input(cli.t("hm.guess.msg")), reg: /\b[#{play_set.gsub('_', '')}]\b/)
-    c_char
+    cli.process_input(cli.user_input(cli.t("hm.guess.msg")), reg: /\b[#{play_set.gsub('_', '')}]\b/)
   end
 
   # @since 1.1.0
@@ -123,12 +111,12 @@ class Hangman
   end
 
   # @since 0.1.6
-  # @version 1.5.0
+  # @version 1.6.0
   def game_loop
     until active_session[:win?] || lives <= 0
       # get user input + input check
       guess = make_guess
-      delete_from_set(c_char)
+      delete_from_set(guess)
       # validate result
       self.lives -= 1 unless matching_character?(guess)
       # update session data
@@ -142,14 +130,10 @@ class Hangman
   end
 
   # @since 0.1.6
-  # @version 1.0.0
+  # @version 1.1.0
   def update_session(guess)
     # Update game state
-    char_idx = 0
-    secret_word.each_char do |char|
-      active_session[:state][char_idx] = guess if guess == char
-      char_idx += 1
-    end
+    s_word.chars.each_with_index { |char, idx| active_session[:state][idx] = guess if guess == char }
     # Update remaining lives
     active_session[:remaining_lives] = lives
     # Check result
@@ -157,16 +141,15 @@ class Hangman
   end
 
   # @since 0.1.6
-  # @version 1.0.0
+  # @version 1.1.0
   def matching_character?(guess)
-    guess.match?(/\b[#{secret_word}]\b/)
+    guess.match?(/\b[#{s_word}]\b/)
   end
 
   # @since 0.1.6
-  # @version 1.1.0
+  # @version 1.2.0
   def announce_result
-    puts active_session[:win?] ? cli.t("hm.win").colorize(:green) : cli.t("hm.lose",
-                                                                          { word: secret_word }).colorize(:red)
+    puts active_session[:win?] ? cli.t("hm.win").colorize(:green) : cli.t("hm.lose", { word: s_word }).colorize(:red)
     active_session[:status] = :ended
     p1.save_game(active_session)
 
